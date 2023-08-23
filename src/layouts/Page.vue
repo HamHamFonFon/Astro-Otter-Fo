088ad22e93851f94e46b3c76fd305cb7ed99f898
Stephane MEAUDRE <smeaudre@kaliop.com>
2023-08-23 13:51:59 +0200

    Add Page layout + store

---
+++ src/layouts/Page.vue
@@ -0,0 +1,36 @@
+<template>
+
+  <!-- Add bar -->
+
+  <!-- Breadcrumbs -->
+
+  <v-main class="main-container"
+          v-touch="{
+      // left: () => (this.$store.commit('customizedTheme/setShowSideBar', false)),
+      // right: () => (this.$store.commit('customizedTheme/setShowSideBar', true))
+    }"
+  >
+    <div class="flex-fill">
+      <slot></slot>
+    </div>
+  </v-main>
+
+  <FooterBar />
+</template>
+
+<script>
+
+import FooterBar from "@/components/Layout/FooterBar.vue";
+
+
+export default {
+  name: "PageLayout",
+  components: {
+    FooterBar
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -5,13 +5,14 @@ import App from './App.vue'
 import router from '@/router';
 
 // Store
-
+import store from "@/store";
 // Vuetify
 import vuetify from "@/plugins/vuetify";
 
 const app = createApp(App);
 
 app.use(router);
+app.use(store);
 app.use(vuetify);
 
 app.mount('#app')
