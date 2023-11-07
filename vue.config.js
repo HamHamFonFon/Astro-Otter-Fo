47a42280f52e347f6ab5e7b63d3171af7d2a97d1
Stephane MEAUDRE <smeaudre@kaliop.com>
2023-11-07 13:51:33 +0100

    Debug host for local env

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,13 @@
 const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    allowedHosts: [
+      'localhost',
+      '127.0.0.1',
+      'www.astro-otter.local',
+      'astro-otter.local'
+    ]
+  }
 })
