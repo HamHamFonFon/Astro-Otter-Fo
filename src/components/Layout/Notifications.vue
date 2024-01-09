ff1c04c1082766c752da3c6a94ca57ba76ff78b8
Stephane MEAUDRE <smeaudre@kaliop.com>
2024-01-09 15:28:40 +0100

    Remove useless code

--- src/components/Layout/Notifications.vue
+++ src/components/Layout/Notifications.vue
@@ -5,17 +5,13 @@ import { VSonner, toast } from 'vuetify-sonner'
 import 'vuetify-sonner/style.css'
 import { mercureConfig } from '@/configs/mercure';
 
-// let notificationsInterval;
-
 const getNotifications = () => {
   const hubUrl = new URL(mercureConfig.url);
   hubUrl.searchParams.append('topic', `${mercureConfig.topic}`);
 
   const eventSource = new EventSource(hubUrl.toString(), { withCredentials: true });
   eventSource.onmessage = (e) => {
-    console.log(e)
     const response = JSON.parse(e.data);
-    console.log(response);
 
     toast(response.message, {
       description: response.date,
@@ -27,8 +23,7 @@ const getNotifications = () => {
     });
   };
 
-  // eventSource.onerror = (e) =>  {
-    // console.log(e);
+  // eventSource.onerror = () =>  {
     // eventSource.close();
   // }
 }
@@ -36,8 +31,6 @@ const getNotifications = () => {
 onMounted(() => {
   getNotifications()
 })
-
-// onBeforeMount(() => clearInterval(notificationsInterval))
 </script>
 
 <template>
