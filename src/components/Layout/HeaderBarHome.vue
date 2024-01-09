3cf1720a157538a583b1e250ac69ca026bb06cf4
Stephane MEAUDRE <smeaudre@kaliop.com>
2024-01-09 15:27:06 +0100

    Remove interval for reading notifications

--- src/components/Layout/HeaderBarHome.vue
+++ src/components/Layout/HeaderBarHome.vue
@@ -2,13 +2,11 @@
 import {defineAsyncComponent} from "vue";
 
 const LanguageSwitcher = defineAsyncComponent(() => import('@/components/Layout/LanguageSwitcher.vue'))
-// const Notifications = defineAsyncComponent(() => import('@/components/Layout/Notifications.vue'))
 </script>
 
 <template>
       <div class="headerHome d-flex">
         <v-spacer></v-spacer>
-<!--        <Notifications btnColor="transparent" iconColor="#1ed760" bgColor="background"></Notifications>-->
         <LanguageSwitcher btnColor="transparent" iconColor="#1ed760" bgColor="background"></LanguageSwitcher>
       </div>
 </template>
--- src/components/Layout/Notifications.vue
+++ src/components/Layout/Notifications.vue
@@ -1,42 +1,43 @@
 <script setup>
-import {onBeforeMount, onMounted} from "vue";
+import {onMounted} from "vue";
 
 import { VSonner, toast } from 'vuetify-sonner'
 import 'vuetify-sonner/style.css'
 import { mercureConfig } from '@/configs/mercure';
 
-let notificationsInterval;
+// let notificationsInterval;
 
 const getNotifications = () => {
   const hubUrl = new URL(mercureConfig.url);
   hubUrl.searchParams.append('topic', `${mercureConfig.topic}`);
 
-  const eventSource = new EventSource(hubUrl.toString(), { withCredentials: false });
-  eventSource.onmessage = ({data}) => {
-    const response = JSON.parse(data);
+  const eventSource = new EventSource(hubUrl.toString(), { withCredentials: true });
+  eventSource.onmessage = (e) => {
+    console.log(e)
+    const response = JSON.parse(e.data);
     console.log(response);
 
     toast(response.message, {
-      description: 'Monday, January 4rd at 6:00pm',
+      description: response.date,
       cardProps: {
         color: 'success'
       },
       prependIcon: 'mdi-check-circle',
-      duration: -1
+      duration: 10000
     });
   };
 
-  eventSource.onerror = (e) =>  {
-    console.log(e);
-    eventSource.close();
-  }
+  // eventSource.onerror = (e) =>  {
+    // console.log(e);
+    // eventSource.close();
+  // }
 }
 
 onMounted(() => {
   getNotifications()
 })
 
-onBeforeMount(() => clearInterval(notificationsInterval))
+// onBeforeMount(() => clearInterval(notificationsInterval))
 </script>
 
 <template>
