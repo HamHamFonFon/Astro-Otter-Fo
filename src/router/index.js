c93f31efc58b726c85617136f258afbfa19584e2
Stephane MEAUDRE <smeaudre@kaliop.com>
2024-01-12 14:45:06 +0100

    POC & demo Mercure

--- src/router/index.js
+++ src/router/index.js
@@ -9,193 +9,201 @@ import backgroundConstellation from '@/assets/images/background/constellations.j
 import backgroundCatalogs from '@/assets/images/background/background_vlt.jpg';
 
 const routes = [
-    {
-        path: '/',
-        name: 'root',
-        redirect: '/home',
-        meta: {}
-    },
-    {
-        path: '/home',
-        name: 'home',
-        component: () => import('@/views/HomePage.vue'),
-        meta: {
-            layout: 'home',
-            key: "menu.home",
-            icon: "mdi-view-dashboard-outline",
-        }
-    },
-    // DSO
-    {
-        path: '/catalogs',
-        name: 'catalogs',
-        component: () => import('@/views/dso/CatalogsPage.vue'),
-        meta: {
-            layout: 'page',
-            key: "menu.catalogs",
-            icon: 'iconDso',
-            image: backgroundCatalogs,
-        }
-    },
-    {
-        path: '/catalog/:id',
-        name: 'dso',
-        component: () => import('@/views/dso/DsoPage.vue'),
-        meta: {
-            layout: 'page',
-            key: "menu.dso",
-        }
-    },
-    // Constellations
-    {
-        path: '/constellations',
-        name: 'constellations',
-        component: () => import('@/views/constellations/ConstellationList.vue'),
-        meta: {
-            layout: 'page',
-            key: "menu.constellations",
-            icon: 'iconConstellation',
-            image: backgroundConstellation,
-        }
-    },
-    {
-        path: '/constelllations/:constellationId/:constellationName',
-        name: 'constellation',
-        component: () => import('@/views/constellations/ConstellationPage.vue'),
-        meta: {
-            layout: 'page',
-            key: '',
-        }
-    },
-    // Astrobin
-    {
-        path: '/astrobin',
-        name: 'astrobin',
-        component: () => import('@/views/astrobin/AstrobinHome.vue'),
-        children: [
-            {
-                path: 'top-picks',
-                name: 'toppicks',
-                // component: () => import(''),
-                meta: {
-                    layout: 'page',
-                    key: "menu.astrobin.toppicks",
-                    text: 'Top-picks',
-                    icon: '',
-                    image: backgroundConstellation
-                }
-            },
-        ],
-        meta: {
-            layout: 'page',
-            key: "menu.astrobin",
-            icon: 'mdi-tooltip-text-outline',
-            text: 'Astrobin API',
-            image: backgroundAstrobin,
-        }
-    },
-    {
-        path: '/astrobin/image-of-the-day',
-        name: 'today',
-        component: () => import('@/views/astrobin/ImageOfTheDay.vue'),
-        meta: {
-            layout: 'page',
-            key: "menu.astrobin.today",
-            text: 'Image of the day',
-            icon: '',
-            image: backgroundIOTD,
-            // seo
-            title: 'Astrobin Image Of The Day',
-            description: 'Display selected image of the day and last ten images of the day',
-        }
-    },
-    {
-        path: '/astrobin/browse-images',
-        name: 'browser',
-        component: () => import('@/views/astrobin/BrowseImages.vue'),
-        meta: {
-            layout: 'page',
-            key: "menu.astrobin.images",
-            text: 'Browse Astrobin API',
-            icon: '',
-            image: backgroundConstellation,
-            title: 'Astrobin images browser',
-            description: 'Search, filter and sort images from Astrobin website',
-
-        }
-    },
-    // Contact
-    {
-        path: '/contact-us',
-        name: 'contact',
-        component: () => import('@/views/pages/Contact.vue'),
-        meta: {
-            layout: 'page',
-            icon: 'mdi-pencil',
-            key: 'menu.contact',
-        }
-    },
-    // Prismic
-    {
-        path: '/pages/:uid',
-        name: 'primisc_content_page',
-        component: () => import('@/views/pages/prismic.vue'),
-        meta: {
-            layout: 'page',
-            key: 'menu.support',
-        }
-    },
-    {
-        path: '/blog/:uid',
-        name: 'primisc_article_page',
-        component: () => import('@/views/pages/prismic.vue'),
-        meta: {
-            layout: 'page',
-            key: 'menu.support',
-        }
-    },
-    {
-        name: 'pathNotFound',
-        path: '/:notFound',
-        component: () => import('@/views/NotFound.vue'),
-        meta: {
-            layout: 'default'
-        }
+  {
+    path: '/',
+    name: 'root',
+    redirect: '/home',
+    meta: {}
+  },
+  {
+    path: '/home',
+    name: 'home',
+    component: () => import('@/views/HomePage.vue'),
+    meta: {
+      layout: 'home',
+      key: "menu.home",
+      icon: "mdi-view-dashboard-outline",
     }
+  },
+  // DSO
+  {
+    path: '/catalogs',
+    name: 'catalogs',
+    component: () => import('@/views/dso/CatalogsPage.vue'),
+    meta: {
+      layout: 'page',
+      key: "menu.catalogs",
+      icon: 'iconDso',
+      image: backgroundCatalogs,
+    }
+  },
+  {
+    path: '/catalog/:id',
+    name: 'dso',
+    component: () => import('@/views/dso/DsoPage.vue'),
+    meta: {
+      layout: 'page',
+      key: "menu.dso",
+    }
+  },
+  // Constellations
+  {
+    path: '/constellations',
+    name: 'constellations',
+    component: () => import('@/views/constellations/ConstellationList.vue'),
+    meta: {
+      layout: 'page',
+      key: "menu.constellations",
+      icon: 'iconConstellation',
+      image: backgroundConstellation,
+    }
+  },
+  {
+    path: '/constelllations/:constellationId/:constellationName',
+    name: 'constellation',
+    component: () => import('@/views/constellations/ConstellationPage.vue'),
+    meta: {
+      layout: 'page',
+      key: '',
+    }
+  },
+  // Astrobin
+  {
+    path: '/astrobin',
+    name: 'astrobin',
+    component: () => import('@/views/astrobin/AstrobinHome.vue'),
+    children: [
+      {
+        path: 'top-picks',
+        name: 'toppicks',
+        // component: () => import(''),
+        meta: {
+          layout: 'page',
+          key: "menu.astrobin.toppicks",
+          text: 'Top-picks',
+          icon: '',
+          image: backgroundConstellation
+        }
+      },
+    ],
+    meta: {
+      layout: 'page',
+      key: "menu.astrobin",
+      icon: 'mdi-tooltip-text-outline',
+      text: 'Astrobin API',
+      image: backgroundAstrobin,
+    }
+  },
+  {
+    path: '/astrobin/image-of-the-day',
+    name: 'today',
+    component: () => import('@/views/astrobin/ImageOfTheDay.vue'),
+    meta: {
+      layout: 'page',
+      key: "menu.astrobin.today",
+      text: 'Image of the day',
+      icon: '',
+      image: backgroundIOTD,
+      // seo
+      title: 'Astrobin Image Of The Day',
+      description: 'Display selected image of the day and last ten images of the day',
+    }
+  },
+  {
+    path: '/astrobin/browse-images',
+    name: 'browser',
+    component: () => import('@/views/astrobin/BrowseImages.vue'),
+    meta: {
+      layout: 'page',
+      key: "menu.astrobin.images",
+      text: 'Browse Astrobin API',
+      icon: '',
+      image: backgroundConstellation,
+      title: 'Astrobin images browser',
+      description: 'Search, filter and sort images from Astrobin website',
+
+    }
+  },
+  // Contact
+  {
+    path: '/contact-us',
+    name: 'contact',
+    component: () => import('@/views/pages/Contact.vue'),
+    meta: {
+      layout: 'page',
+      icon: 'mdi-pencil',
+      key: 'menu.contact',
+    }
+  },
+  // Prismic
+  {
+    path: '/pages/:uid',
+    name: 'primisc_content_page',
+    component: () => import('@/views/pages/prismic.vue'),
+    meta: {
+      layout: 'page',
+      key: 'menu.support',
+    }
+  },
+  {
+    path: '/blog/:uid',
+    name: 'primisc_article_page',
+    component: () => import('@/views/pages/prismic.vue'),
+    meta: {
+      layout: 'page',
+      key: 'menu.support',
+    }
+  },
+  {
+    path: '/mercure-demo',
+    name: 'poc-mercure',
+    component: () => import('@/views/pages/mercure.vue'),
+    meta: {
+      layout: 'default'
+    }
+  },
+  {
+    name: 'pathNotFound',
+    path: '/:notFound',
+    component: () => import('@/views/NotFound.vue'),
+    meta: {
+      layout: 'default'
+    }
+  }
 ];
 
 const router = createRouter({
-    history: createWebHistory(process.env.BASE_URL),
-    routes,
-    scrollBehavior() {
-        return { left: 0, top: 0 }
-    }
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
+  scrollBehavior() {
+    return { left: 0, top: 0 }
+  }
 })
 
 router.beforeEach(async (to, from, next) => {
-    // Set refresh JWT token for requests
-    const timestamp = new Date().getTime()
-    if (true === store.getters["auth/isLoggedIn"]) {
-        /**
-         * TODO fix login/refresh
-         * @type {*|null}
-         */
-        let expireTokenDate = store.getters["auth/getJwtExp"].exp ?? null;
-        if (expireTokenDate && expireTokenDate > timestamp) {
-            localStorage.clear();
-            //await refreshToken();
-            await login();
-        } else {
-            await login();
-        }
+  // Set refresh JWT token for requests
+  const timestamp = new Date().getTime()
+  if (true === store.getters["auth/isLoggedIn"]) {
+    /**
+     * TODO fix login/refresh
+     * @type {*|null}
+     */
+    let expireTokenDate = store.getters["auth/getJwtExp"].exp ?? null;
+    if (expireTokenDate && expireTokenDate > timestamp) {
+      localStorage.clear();
+      //await refreshToken();
+      await login();
     } else {
-        await login();
+      await login();
     }
+  } else {
+    await login();
+  }
 
-    if (true === to.meta.requiresAuth) {
-        next("login");
-    }
-    next();
+  if (true === to.meta.requiresAuth) {
+    next("login");
+  }
+  next();
 });
 
 export default router
--- src/views/pages/mercure.vue
+++ src/views/pages/mercure.vue
@@ -1,11 +1,139 @@
 <script setup>
 
+import {onMounted, reactive, ref, watch} from "vue";
+import { mercureConfig } from '@/configs/mercure';
+import api from "@/configs/api";
+
+const userName = ref(null);
+const notifications = reactive([]);
+const usersNotifications = reactive([]);
+
+const getNotifications = () => {
+  const url = new URL(mercureConfig.url);
+  url.searchParams.append('topic', `${api.API_HOST}/demo`);
+
+  const eventSource = new EventSource(url.toString(), { withCredentials: true});
+  eventSource.onmessage = (e) => {
+    const response = JSON.parse(e.data);
+    notifications.push(response);
+  }
+}
+
+const getNotificationsForUser = () => {
+  usersNotifications.length = 0;
+  const urlByUser = new URL(mercureConfig.url);
+  urlByUser.searchParams.append('topic', `${api.API_HOST}/demo-user/${userName.value}`);
+
+  const eventSource = new EventSource(urlByUser.toString(), { withCredentials: true});
+  eventSource.onmessage = (e) => {
+    const response = JSON.parse(e.data);
+    usersNotifications.push(response);
+  }
+}
+
+const deleteItem = (index) =>  notifications.splice(index, 1);
+const deleteUserItem = (index) => usersNotifications.splice(index, 1);
+
+onMounted(() => {
+  getNotifications();
+
+});
+
+watch(userName, () => getNotificationsForUser())
 </script>
 
 <template>
+  <h1>MERCURE DEMO</h1>
+
+  <div class="grid-container">
+
+    <div class="grid-child">
+      <h2>Global notifications</h2>
+      <div class="Message" v-for="(notification, i) in notifications" v-bind:key="notification">
+        <div class="Message-body">
+          <p>{{ notification.message }}</p>
+        </div>
+        <button class="Message-close js-messageClose" @click="deleteItem(i)"><v-icon icon="mdi-close"></v-icon></button>
+      </div>
+    </div>
+
+    <div class="grid-child">
+      <div>
+        <h2>Notification for user {{ userName }}</h2>
+        <v-text-field
+          v-model="userName"
+          variant="outlined"
+          color="white"
+          clearable
+        >
+        </v-text-field>
+      </div>
+      <hr />
+      <div class="Message" v-for="(notification, i) in usersNotifications" v-bind:key="notification">
+        <div class="Message-body">
+          <p>{{ notification.message }}</p>
+        </div>
+        <button class="Message-close js-messageClose" @click="deleteUserItem(i)"><v-icon icon="mdi-close"></v-icon></button>
+      </div>
+    </div>
+  </div>
+
 
 </template>
 
-<style scoped>
+<style scoped lang="scss">
+.grid-container {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 20px;
+}
 
+.grid-child {
+  border: 1px solid white;
+  padding: 0.5em;
+}
+
+.Message {
+  display: table;
+  position: relative;
+  margin: 1em auto 0;
+  width: 500px;
+  border-radius: 5px;
+  background-color: #3E8ED0;
+  color: #F3FDFB;
+  transition: all 0.2s ease;
+
+  &.is-hidden {
+    opacity: 0;
+    height: 0;
+    font-size: 0;
+    padding: 0;
+    margin: 0 auto;
+    display: block;
+  }
+}
+
+.Message-body {
+  display: table-cell;
+  vertical-align: middle;
+  padding: 1em 20px;
+  color: #fff;
+}
+
+.Message-close {
+  padding: 0.25px;
+  border-radius: 50%;
+  position: absolute;
+  background-color: rgba(black, 0.3);
+  color: #fff;
+  border: none;
+  outline: none;
+  font-size: 12px;
+  right: 5px;
+  top: 5px;
+  cursor: pointer;
+  &:hover {
+    background-color: rgba(black, 0.5);
+  }
+}
 </style>
